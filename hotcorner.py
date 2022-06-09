a79dcf7a117ecaa87a95c18a6b53b140940460cf
Rohith Vishaal <rohith.vishaal@gmail.com>
2022-06-09 17:09:01 +0530

    change for windows

--- hotcorner.py
+++ hotcorner.py
@@ -1,4 +1,5 @@
-from pyautogui import position, press
+from pyautogui import position, press, hold
+from os import platform
 from time import sleep
 import pyautogui
 pyautogui.FAILSAFE = False
@@ -6,5 +7,9 @@ while True:
     x,y = position()
     sleep(0.01)
     if (x<=0 or x <=2) and (y<=0 or y<=2):
-        press('win')
+        if platform == 'Linux':
+            press('win')
+         else:
+            with hold('win'):
+                press('tab')
         sleep(1)
